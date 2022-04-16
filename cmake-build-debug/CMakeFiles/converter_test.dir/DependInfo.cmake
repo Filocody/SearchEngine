35c152fa8a65b6100de0a3b75ea2450b3304b0fa
Ilya Fedotov <stigiec001@gmail.com>
2022-04-16 13:04:24 +0300

    Added gtests, converterJSON final version, InvertedIndex under process

---
+++ cmake-build-debug/CMakeFiles/converter_test.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/stigi/CLionProjects/search_engine/TestConverterJSON.cpp" "C:/Users/stigi/CLionProjects/search_engine/cmake-build-debug/CMakeFiles/converter_test.dir/TestConverterJSON.cpp.obj"
+  "C:/Users/stigi/CLionProjects/search_engine/converterJSON.cpp" "C:/Users/stigi/CLionProjects/search_engine/cmake-build-debug/CMakeFiles/converter_test.dir/converterJSON.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "JSON_DIAGNOSTICS=0"
+  "JSON_USE_IMPLICIT_CONVERSIONS=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../nlohmann_json/single_include"
+  "_deps/googletest-src/googletest/include"
+  "_deps/googletest-src/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/stigi/CLionProjects/search_engine/cmake-build-debug/_deps/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "C:/Users/stigi/CLionProjects/search_engine/cmake-build-debug/_deps/googletest-build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/search_engine.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/stigi/CLionProjects/search_engine/InvertedIndex.cpp" "C:/Users/stigi/CLionProjects/search_engine/cmake-build-debug/CMakeFiles/search_engine.dir/InvertedIndex.cpp.obj"
+  "C:/Users/stigi/CLionProjects/search_engine/converterJSON.cpp" "C:/Users/stigi/CLionProjects/search_engine/cmake-build-debug/CMakeFiles/search_engine.dir/converterJSON.cpp.obj"
+  "C:/Users/stigi/CLionProjects/search_engine/main.cpp" "C:/Users/stigi/CLionProjects/search_engine/cmake-build-debug/CMakeFiles/search_engine.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "JSON_DIAGNOSTICS=0"
+  "JSON_USE_IMPLICIT_CONVERSIONS=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "_deps/googletest-src/googletest/include"
+  "_deps/googletest-src/googletest"
+  "../nlohmann_json/single_include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
